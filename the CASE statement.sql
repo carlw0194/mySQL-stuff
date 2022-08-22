fcc7ce94862fe12c8dde87a16bd6f5413cda01cb
Carlton Njong <96522018+carlw0194@users.noreply.github.com>
2022-08-22 19:47:50 +0300

    add another case statement example

--- the CASE statement.sql
+++ the CASE statement.sql
@@ -5,11 +5,40 @@
  Extract a dataset containing the following information about the managers: employee number, first name, and last name. Add two columns at the end – one showing the difference between the maximum and minimum salary of that employee, and another one saying whether this salary raise was higher than $30,000 or NOT.
  */
  use employees;
- select e.first_name,e.last_name,e.emp_no,
- max(s.salary) - min(s.salary) as salary_difference,
- case when  max(s.salary) - min(s.salary) >30000 then 'raised by more than 30k'
- when max(s.salary) - min(s.salary) = 30000 then 'raised by exactly 30k' 
- else 'raised by less than 30k'
- end as salary_comment from employees e join dept_manager dm on e.emp_no = dm.emp_no
- join salaries s on s.emp_no = dm.emp_no
- group by emp_no order by emp_no;
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.emp_no,
+    MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    CASE
+        WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'raised by more than 30k'
+        WHEN MAX(s.salary) - MIN(s.salary) = 30000 THEN 'raised by exactly 30k'
+        ELSE 'raised by less than 30k'
+    END AS salary_comment
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+        JOIN
+    salaries s ON s.emp_no = dm.emp_no
+GROUP BY emp_no
+ORDER BY emp_no;
+ 
+ # Example 2
+ /*
+Extract the employee number, first name, and last name of the first 100 employees, and add a fourth column, called “current_employee” saying “Is still employed” if the employee is still working in the company, or “Not an employee anymore” if they aren’t. 
+ */
+ 
+ SELECT 
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN MAX(de.to_date) > SYSDATE() THEN 'Is still employed'
+        ELSE 'Not an employee anymore'
+    END AS current_employee
+FROM
+    employees e
+        JOIN
+    dept_emp de ON de.emp_no = e.emp_no
+GROUP BY de.emp_no
+LIMIT 100;
